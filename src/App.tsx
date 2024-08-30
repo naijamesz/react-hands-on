53815853f80cb75ba7ae6f98b5f6e33162494f45
naijamesz <kodlikejamesz@hotmail.com>
2024-08-30 21:35:59 +0700

    completed Lab1-4

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,10 @@
 import './App.css';
+import PorjectsPage from './projects/PorjectsPage';
 
 function App() {
   return (
     <>
+      <PorjectsPage />
       <blockquote cite='Benjamin Franklin'>
         Tell me and I forget, teach me and I may remember, involve me and I learn.
       </blockquote>
---
+++ src/projects/PorjectsPage.tsx
@@ -0,0 +1,5 @@
+import React from 'react';
+
+export default function PorjectsPage() {
+  return <div>PorjectsPage</div>;
+}
