3d4568942359618ef4f7b54cd277b66803aa5c53
naijamesz <kodlikejamesz@hotmail.com>
2024-08-30 22:17:48 +0700

    completed lab-7 listdata

--- src/projects/ProjectList.tsx
+++ src/projects/ProjectList.tsx
@@ -4,5 +4,22 @@ interface ProjectListProps {
   projects: Project[];
 }
 export default function ProjectList({ projects }: ProjectListProps) {
-  return <pre>{JSON.stringify(projects, null, ' ')}</pre>;
+  return (
+    <div className='container'>
+      {projects.map(project => (
+        <div key={project.id} className='cols-sm'>
+          <div className='card'>
+            <img src={project.imageUrl} alt={project.name} />
+            <section className='section dark'>
+              <h5 className='strong'>
+                <strong>{project.name}</strong>
+              </h5>
+              <p>{project.description}</p>
+              <p>Budget : {project.budget.toLocaleString('TH', { style: 'currency', currency: 'THB' })}</p>
+            </section>
+          </div>
+        </div>
+      ))}
+    </div>
+  );
 }
