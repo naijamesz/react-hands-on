9b0197a4f08b02fce2d74ed1a1e4be074865ef13
naijamesz <kodlikejamesz@hotmail.com>
2024-08-30 23:58:06 +0700

    completed lab-16 form validation

--- src/projects/ProjectForm.tsx
+++ src/projects/ProjectForm.tsx
@@ -8,8 +8,17 @@ interface ProjectFormProps {
 
 export default function ProjectForm({ onCancel, onSave, project: initialProject }: ProjectFormProps) {
   const [project, setProject] = useState(initialProject);
+
+  // form validation
+  const [errors, setErrors] = useState({
+    name: '',
+    description: '',
+    budget: '',
+  });
+
   const handleSubmit = (event: SyntheticEvent) => {
     event.preventDefault();
+    if (!isValid()) return;
     // onSave(new Project({ name: 'Updated Project' }));
     onSave(project);
   };
@@ -38,11 +47,37 @@ export default function ProjectForm({ onCancel, onSave, project: initialProject 
       updatedProject = new Project({ ...p, ...change });
       return updatedProject;
     });
+    setErrors(() => validate(updatedProject));
   };
+
+  function validate(project: Project) {
+    let errors: any = { name: '', description: '', budget: '' };
+    if (project.name.length === 0) {
+      errors.name = 'Na,e is required';
+    }
+    if (project.name.length > 0 && project.name.length < 3) {
+      errors.name = 'Name needs to be at least 3 characters.';
+    }
+    if (project.description.length === 0) {
+      errors.description = 'Description is required';
+    }
+    if (project.budget === 0) {
+      errors.budget = 'Budget must be more than $0';
+    }
+    return errors;
+  }
+  function isValid() {
+    return errors.name.length === 0 && errors.description.length === 0 && errors.budget.length === 0;
+  }
   return (
     <form className='input-group vertical' onSubmit={handleSubmit}>
       <label htmlFor='name'>Project Name</label>
       <input type='text' name='name' placeholder='enter name' value={project.name} onChange={handleChange} />
+      {errors.name.length > 0 && (
+        <div className='card error'>
+          <p>{errors.name}</p>
+        </div>
+      )}
       <label htmlFor='description'>Project Description</label>
 
       <textarea
@@ -51,9 +86,19 @@ export default function ProjectForm({ onCancel, onSave, project: initialProject 
         value={project.description}
         onChange={handleChange}
       />
+      {errors.description.length > 0 && (
+        <div className='card error'>
+          <p>{errors.description}</p>
+        </div>
+      )}
       <label htmlFor='budget'>Project Budget</label>
 
       <input type='number' name='budget' placeholder='enter budget' value={project.budget} onChange={handleChange} />
+      {errors.budget.length > 0 && (
+        <div className='card error'>
+          <p>{errors.budget}</p>
+        </div>
+      )}
       <label htmlFor='isActive'>Active?</label>
       <input type='checkbox' name='isActive' checked={project.isActive} onChange={handleChange} />
 
