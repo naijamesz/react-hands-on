33e7466e7594972a843cc685052da82133409da5
naijamesz <kodlikejamesz@hotmail.com>
2024-08-30 23:34:33 +0700

    completed lab-15 form data to state

--- src/projects/PorjectsPage.tsx
+++ src/projects/PorjectsPage.tsx
@@ -1,17 +1,24 @@
+import { useState } from 'react';
 import { Fragment } from 'react/jsx-runtime';
 import { MOCK_PROJECTS } from './MockProject';
 import { Project } from './Project';
 import ProjectList from './ProjectList';
 
 export default function PorjectsPage() {
+  const [projects, setProjects] = useState<Project[]>(MOCK_PROJECTS);
   const saveProject = (project: Project) => {
-    console.log('Saving project: ', project);
+    // console.log('Saving project: ', project);
+    let updatedProjects = projects.map((p: Project) => {
+      return p.id === project.id ? project : p;
+    });
+    setProjects(updatedProjects);
   };
   return (
     <Fragment>
       {/* pre = preformatted */}
       <h1>Projects</h1>
-      <ProjectList onSave={saveProject} projects={MOCK_PROJECTS} />
+      <ProjectList onSave={saveProject} projects={projects} />
+      {/* <ProjectList onSave={saveProject} projects={MOCK_PROJECTS} /> */}
     </Fragment>
   );
 }
